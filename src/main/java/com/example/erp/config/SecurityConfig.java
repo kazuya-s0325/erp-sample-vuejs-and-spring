bfdcf0ec3d913993dd3713fb21db1a3a70fdcd17
kazuya-s0325 <kaz.keyboardist@gmail.com>
2023-07-17 08:56:07 +0900

    disable spring security

---
+++ src/main/java/com/example/erp/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+// WebSecurityConfigureAdapter is deprecated
+package com.example.erp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.build();
+  }
+}
